53bbd65a184daacee72fd5635fa25cf9a5618c6f
Gornostalev <30836610+Gornostalev@users.noreply.github.com>
2025-02-01 19:47:05 +0500

    add event code

--- AvaloniaApplication1/Views/MainView.axaml.cs
+++ AvaloniaApplication1/Views/MainView.axaml.cs
@@ -12,6 +12,15 @@ public partial class MainView : UserControl
     }
     private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
-        Debug.WriteLine("Click!");
+        if(double.TryParse(Celcius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            Forengeits.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Celcius.Text = "0";
+            Forengeits.Text = "0";
+        }
     }
 }
